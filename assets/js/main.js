92a5df64a6efc639b2db882644ec42fca8b9ed30
unknown <kumar05.962013@gmail.com>
2018-06-22 20:52:22 +0530

    1st

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+function searchData(){
+	var input,tr,td,filter,table,i;
+	input=document.getElementById('input');
+	filter=input.value.toUpperCase();
+	table=document.getElementById('table');
+	tr=table.getElementsByTagName('tr');
+	for( i = 0; i<tr.length;i++) {
+		td=tr[i].getElementsByTagName("td")[0];
+		td1=tr[i].getElementsByTagName("td")[1];
+		td2=tr[i].getElementsByTagName("td")[2];
+		td3=tr[i].getElementsByTagName("td")[3];
+		if (td+td1+td2+td3) {
+			if (td.innerHTML.toUpperCase().indexOf(filter)+td1.innerHTML.toUpperCase().indexOf(filter)+td2.innerHTML.toUpperCase().indexOf(filter)+td3.innerHTML.toUpperCase().indexOf(filter) > -4) {
+				tr[i].style.display="";
+			} else {
+				tr[i].style.display="none";
+			}
+		}
+	}
+}
+        // change table row color 
+var table,tr,i;
+table=document.getElementById("table");
+tr=table.getElementsByTagName("tr");
+for(i = 0;i<tr.length;i++) {
+	if (i % 2 === 0) {
+		tr[i].style.background='#e8edff';
+	}
+}
